20ebe370f93d83ecab9b7c7059ee6c3778e8860d
thomas <tom.angotti11@gmail.com>
2022-12-17 22:14:43 -0700

    commit tree bb4361e759c2b15367649a72ccedcd428c0a737a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+const record = [
+    {
+        year: "2015", 
+        result: "W"
+    },
+    {
+        year: "2014", 
+        result: "N/A"
+    },
+    {
+        year: "2013", 
+        result: "L"
+    },
+]
+
+function superbowlWin(record){
+   for (const user of record){
+    if(user.result === "W"){
+        return user.year
+    }
+   }
+
+}
+
+console.log(superbowlWin(record))
